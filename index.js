35964c3727f36b43c645a82f9b0b27bcf9966c88
czeslaw6300149 <czeslaw6300149@gmail.com>
2017-08-18 10:50:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2327,7 +2327,7 @@ function tarotCardReply(count) {
 
 function TempName() {
 	let TempName = ['水無月 流歌'];
-	return TempName[Math.floor((Math.random() * (TempName.length)) + 0)]
+	return TempName[Math.floor((Math.random() * (TempName.length)) + 0)];
 }
 
 //return rplyHead[Math.floor((Math.random() * (rplyHead.length)) + 0)] + '\n' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
@@ -2337,7 +2337,8 @@ function WhatToEat(){ //吃啥
 	var FoodArr = Food.split(",");
 	let Sentence = ['恩～肚子餓餓要不要試試看'+ FoodArr[0] +'這個如何?\n' + FoodArr[1]];
 	
-	return Sentence[Math.floor((Math.random() * (Sentence.length)) + 0)]
+	//return Sentence[Math.floor((Math.random() * (Sentence.length)) + 0)];
+	return FoodArr[0] + '=' + FoodArr[1];
 }
 
 function WhatToPlay(){ //玩啥
